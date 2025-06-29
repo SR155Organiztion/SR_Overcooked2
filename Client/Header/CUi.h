941f6b52fb96ea7371bf8abfa0cf6419191eeb51
beobi389 <ing5906@naver.com>
2025-06-29 18:34:30 +0900

    [Feat] 문서용 주석 test
    독시젠 문서용 주석 추가 test

--- Client/Header/CUi.h
+++ Client/Header/CUi.h
@@ -1,6 +1,16 @@
 #pragma once
 #include "CGameObject.h"
 #include "Engine_Define.h"
+/**
+* @mainpage
+* - 소개: UI 부모 클래스
+* @date 2025-06-29
+* @section CREATINFO 작성정보
+* - 작성자: 김서영
+* - 작성일: 2025.06.29
+* @section MODIFYINFO 수정 정보
+* - 수정일/ 수정자: 수정내역
+*/
 
 class CUi :public CGameObject
 {
--- Client/Header/CUi_Button.h
+++ Client/Header/CUi_Button.h
@@ -3,6 +3,19 @@
 
 class CUi_Button :   public CUi
 {
+	
+	/**
+	* @file CUi_Button
+	* @date 2025.06.29
+	* @author 김서영
+	* @brief 버튼 정보를 저장하는 클래스
+	*/
+
+
+	/**
+	* @struct BUTTON_TYPE
+	* @brief 버튼 종류를 나타내는 열거체
+	*/
 	enum BUTTON_TYPE 
 	{START_BUTTON, CONTINUE_BUTTON, STATE_BUTTON, OK_BUTTON, NO_BUTTON, END_BUTTON};
 
@@ -30,12 +43,24 @@ protected:
 	~CUi_Button();
 
 public:
+	/**
+	* @brief BUTTON_TYPE에 따라 이미지 정보를 불러오는 함수로 매개변수로 받은 BUTTON_TYPE을 m_eType에 저장한다.
+	*/
 	void Ready_GameObject(LPDIRECT3DDEVICE9 pGraphicDev, BUTTON_TYPE _cValue);
+	/**
+	* @brief m_etype에 저장된 BUTTON_TYPE에 따라 다른 기능이 동작. (마우스 피킹, 키보드 입력 등)
+	*/
 	void Update_GameObject();
 	void LateUpdate_GameObject();
 	void Render_GameObject();
 
+	/**
+	* @brief 마우스 클릭 시, 마우스가 피킹된 버튼 타입에 따라 이미지 정보나 씬 전환등을 업데이트
+	*/
 	void Mouse_Pickig(int _m_iKeyPress);
+	/**
+	* @brief 키입력 시, 입력값에 따라 이미지 정보나 씬 전환등을 업데이트 한다.
+	*/
 	void Key_Input(int m_iMousPicking);
 
 private:
