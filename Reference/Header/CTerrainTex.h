bf8ac6514a386ed0e62cb05e9ee7e989d70f632e
na4742 <126836684+na4742@users.noreply.github.com>
2025-06-30 10:45:43 +0900

    [Feat] : Terrain 추가
    테스트 및 지형충돌 작업을 하기 위한 Terrain

--- Reference/Header/CTerrainTex.h
+++ Reference/Header/CTerrainTex.h
@@ -3,7 +3,7 @@
 
 BEGIN(Engine)
 
-class ENGINE_DLL CTerrainTex :  public CVIBuffer
+class ENGINE_DLL CTerrainTex : public CVIBuffer
 {
 private:
 	explicit CTerrainTex();
@@ -12,26 +12,28 @@ private:
 	virtual ~CTerrainTex();
 
 public:
-	const _vec3* Get_VtxPos() const { return m_pPos; }
-
-public:
 	HRESULT	Ready_Buffer(const _ulong& dwCntX, const _ulong& dwCntZ, const _ulong& dwVtxItv);
-	virtual void	Render_Buffer();
+	virtual void Render_Buffer();
+	const VTXTEX* Get_VTXTEX() const { return m_pVertex; }
 
 private:
 	HANDLE				m_hFile;
 	BITMAPFILEHEADER	m_fH;
 	BITMAPINFOHEADER	m_iH;
 
-	_vec3*				m_pPos;
+	_ulong				m_dwCntX;
+	_ulong				m_dwCntZ;
+	_ulong				m_dwVtxItv;
 
-
+	VTXTEX* m_pVertex;
+	INDEX32* m_pIndex;
 public:
 	virtual CComponent* Clone() override;
+
 	static CTerrainTex* Create(LPDIRECT3DDEVICE9 pGraphicDev,
-								const _ulong& dwCntX, 
-								const _ulong& dwCntZ, 
-								const _ulong& dwVtxItv);
+		const _ulong& dwCntX,
+		const _ulong& dwCntZ,
+		const _ulong& dwVtxItv);
 
 private:
 	virtual void		Free();
