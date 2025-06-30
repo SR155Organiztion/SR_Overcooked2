4f0c5eed6354257aaf9da58aa3a184d958f895e8
yeji-gwon <ehclemf0526@gmail.com>
2025-06-30 14:20:54 +0900

    [Feat] : 환경 오브젝트(양배추, 조리대) 더미 띄우기
    - Ingredient > Lettuce 평면 띄우기
    - Station > EmtpyStation 큐브 띄우기

--- Client/Code/CEmptyStation.cpp
+++ Client/Code/CEmptyStation.cpp
@@ -1,5 +1,7 @@
 #include "pch.h"
 #include "CEmptyStation.h"
+#include "CProtoMgr.h"
+#include "CRenderer.h"
 
 CEmptyStation::CEmptyStation(LPDIRECT3DDEVICE9 pGraphicDev)
 	: CInteract(pGraphicDev)
@@ -17,33 +19,76 @@ CEmptyStation::~CEmptyStation()
 
 HRESULT CEmptyStation::Ready_GameObject()
 {
+	if (FAILED(Add_Component()))
+		return E_FAIL;
+
+	m_pTransformCom->Set_Pos(4.f, 0.f, 4.f);
+
 	return S_OK;
 }
 
 _int CEmptyStation::Update_GameObject(const _float& fTimeDelta)
 {
-	return 0;
+	int iExit = Engine::CGameObject::Update_GameObject(fTimeDelta);
+
+	CRenderer::GetInstance()->Add_RenderGroup(RENDER_ALPHA, this);
+
+	return iExit;
 }
 
 void CEmptyStation::LateUpdate_GameObject(const _float& fTimeDelta)
 {
+	Engine::CGameObject::LateUpdate_GameObject(fTimeDelta);
 }
 
 void CEmptyStation::Render_GameObject()
 {
+	m_pGraphicDev->SetTransform(D3DTS_WORLD, m_pTransformCom->Get_World());
+
+	m_pTextureCom->Set_Texture(0);
+
+	m_pBufferCom->Render_Buffer();
 }
 
 HRESULT CEmptyStation::Add_Component()
 {
+	CComponent* pComponent = nullptr;
+
+	pComponent = m_pBufferCom = dynamic_cast<Engine::CCubeTex*>(CProtoMgr::GetInstance()->Clone_Prototype(L"Proto_CubeTex"));
+	if (nullptr == pComponent)
+		return E_FAIL;
+	m_mapComponent[ID_STATIC].insert({ L"Com_Buffer", pComponent });
+
+	pComponent = m_pTransformCom = dynamic_cast<Engine::CTransform*>(CProtoMgr::GetInstance()->Clone_Prototype(L"Proto_Transform"));
+	if (nullptr == pComponent)
+		return E_FAIL;
+	m_mapComponent[ID_DYNAMIC].insert({ L"Com_Transform", pComponent });
+
+	pComponent = m_pTextureCom = dynamic_cast<Engine::CTexture*>(CProtoMgr::GetInstance()->Clone_Prototype(L"Proto_StationBoxTexture_EmptyStation"));
+	if (nullptr == pComponent)
+		return E_FAIL;
+	m_mapComponent[ID_DYNAMIC].insert({ L"Com_Texture", pComponent });
+
 	return S_OK;
 }
 
-CEmptyStation* CEmptyStation::Create(LPDIRECT3DDEVICE9 pGraphicDev) {
-	return nullptr;
+CEmptyStation* CEmptyStation::Create(LPDIRECT3DDEVICE9 pGraphicDev) 
+{
+	CEmptyStation* pEmptyStation = new CEmptyStation(pGraphicDev);
+
+	if (FAILED(pEmptyStation->Ready_GameObject()))
+	{
+		Safe_Release(pEmptyStation);
+		MSG_BOX("Station_Emtpy Create Failed");
+		return nullptr;
+	}
+
+	return pEmptyStation;
 }
 
 void CEmptyStation::Free()
 {
+	Engine::CGameObject::Free();
 }
 
 _bool CEmptyStation::Get_CanPlace(ICarry* pCarry) const
