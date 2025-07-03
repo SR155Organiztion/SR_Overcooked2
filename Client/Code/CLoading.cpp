ab2637bd240298e4824850f1b362ab730e4d5aca
JunHyeongAn <sure083080@gmail.com>
2025-07-03 15:39:31 +0900

    [Fix] : Stash 내용 적용 stash 내용 적용

--- Client/Code/CLoading.cpp
+++ Client/Code/CLoading.cpp
@@ -100,11 +100,7 @@ _uint CLoading::Loaing_ForStage()
 	lstrcpy(m_szLoading, L"Sprite Component Loading...........................");
 
 	if (FAILED(CProtoMgr::GetInstance()->Ready_Prototype
-<<<<<<< Updated upstream
 	(L"Proto_Button", Engine::CSprite::Create(m_pGraphicDev, L"../Bin/Resource/Texture/UI/Button/MainButton%d.png", SPRITE_NON, 14))))
-=======
-	(L"Proto_Ui", Engine::CSprite::Create(m_pGraphicDev, L"../Bin/Resource/Texture/UI/Button/MainButton%d.png", SPRITE_BUTTON, 4))))
->>>>>>> Stashed changes
 		return E_FAIL;
 
 	////////////////////////////////////////////////////
