a17c36882769e1bc2153efa5cd06bcc58df207b9
beobi389 <ing5906@naver.com>
2025-07-06 14:03:39 +0900

    [Feat] Score UI 더미 추가
    - Score UI 더미 추가
    - 오타 수정
    - 클래스 분류 폴더 이동

--- Client/Header/CUi_Score.h
+++ Client/Header/CUi_Score.h
@@ -1,23 +1,24 @@
 #pragma once
 #include "CUi_Gauge.h"
-class CUi_Scoer :public CUi_Gauge
+class CUi_Score :public CUi_Gauge
 {
 
 private:
 	LPD3DXFONT m_pFont;
 	LPD3DXSPRITE m_pSprite;
+	int m_iScore;
 
 public:
-	CUi_Scoer();
-	CUi_Scoer(LPDIRECT3DDEVICE9 _pGraphicDev);
-	CUi_Scoer(const CGameObject& _rhs);
-	~CUi_Scoer();
+	CUi_Score();
+	CUi_Score(LPDIRECT3DDEVICE9 _pGraphicDev);
+	CUi_Score(const CGameObject& _rhs);
+	~CUi_Score();
 
 public:
 	HRESULT Ready_GameObject(LPDIRECT3DDEVICE9 _m_pGraphicDev, GAUGE_TYPE _type);
-	void Update_GameObject();
+	int Update_GameObject(const _float& _fTimeDelta);
 	void LateUpdate_GameObject();
-	virtual void Render_GameObject(LPDIRECT3DDEVICE9 _m_pGraphicDev);
+	void Render_GameObject();
 	
 private:
 	void Free();
