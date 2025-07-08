13e5dc1dcb8e55c31736514c30ee4b0dfcde3b6b
beobi389 <ing5906@naver.com>
2025-07-08 21:41:59 +0900

    [Feat] Order UI 종류 추가
    Order UI 종류 추가

--- Client/Code/CStage.cpp
+++ Client/Code/CStage.cpp
@@ -327,10 +327,13 @@ HRESULT CStage::Ready_UI_Layer(const _tchar* pLayerTag)
        return E_FAIL;
 
     //레시피
-    pGameObject = CUi_Factory<CUi_Order>::Ui_Create(m_pGraphicDev, BOX_OBJECT);
-    if (nullptr == pGameObject) return E_FAIL;
-    if (FAILED(pLayer->Add_GameObject(L"Ui_Object8", pGameObject)))
-        return E_FAIL;
+    for (int i= 0; i < 4; ++i)
+    {
+        pGameObject = CUi_Factory<CUi_Order>::Ui_Create(m_pGraphicDev);
+        if (nullptr == pGameObject) return E_FAIL;
+        if (FAILED(pLayer->Add_GameObject(L"Ui_Object8", pGameObject)))
+            return E_FAIL;
+    }
 
     /*pGameObject = CUi_Factory<CUi_Order>::Ui_Create(m_pGraphicDev, GAUGE_OBJECT);
     if (nullptr == pGameObject) return E_FAIL;
