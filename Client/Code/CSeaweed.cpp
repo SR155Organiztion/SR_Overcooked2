39b7a285bc4aa1e0f7462d44f98c97cb737a5523
JunHyeongAn <sure083080@gmail.com>
2025-07-11 17:24:52 +0900

    [Fix] : Make_Icon 호출위치 수정 - Ready_xxx함수는 해당 Scene의 Create함수에서 호출됨 - 따라서 StageLoading의 Layer에서 찾아오고 있었음. - 어차피 매 프레임 위치를 이동시켜야 하기 때문에 update문으로 이동 시킴 - 위치값은 잘들어가고 Make_Icon함수도 잘 실행 된걸 확인 했으나 아이콘은 랜더링 되지 않음.

--- Client/Code/CSeaweed.cpp
+++ Client/Code/CSeaweed.cpp
@@ -42,10 +42,6 @@ HRESULT CSeaweed::Ready_GameObject()
 	m_stOpt.bApplyBouncing = false;
 	m_stOpt.bApplyKnockBack = true;
 
-	pGameObject = CManagement::GetInstance()->Get_GameObject(L"UI_Layer", L"Ui_Object9");
-	if (nullptr == pGameObject) 
-		return E_FAIL;
-
 	//dynamic_cast<CUi_Icon*>(pGameObject)->Make_Icon(CIngredient::SEAWEED, { 2.f, m_pTransformCom->Get_Scale().y + 5.f, 2.f });
 	//if (FAILED(pLayer->Add_GameObject(L"Ui_Object9", pGameObject)))
 	//	return E_FAIL;
@@ -60,6 +56,15 @@ _int CSeaweed::Update_GameObject(const _float& fTimeDelta)
 	int iExit = Engine::CGameObject::Update_GameObject(fTimeDelta);
 
 	CRenderer::GetInstance()->Add_RenderGroup(RENDER_ALPHA, this);
+
+	pGameObject = CManagement::GetInstance()->Get_GameObject(L"UI_Layer", L"Ui_Object9");
+	if (nullptr == pGameObject)
+		return E_FAIL;
+
+	_vec3 vPos;
+	m_pTransformCom->Get_Info(INFO_POS, &vPos);
+
+	dynamic_cast<CUi_Icon*>(pGameObject)->Make_Icon(INGREDIENT_TYPE::SEAWEED, vPos);
 
 	if (m_pCurrentState)
 		m_pCurrentState->Update_State(this, fTimeDelta);
