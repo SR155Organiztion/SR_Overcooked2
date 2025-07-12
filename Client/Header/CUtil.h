f6fea7a54502483d794e75d5db7e61c4fb85a8e9
yugi0714 <yugi0714@gmail.com>
2025-07-12 15:17:21 +0900

    [Feat] : ObjectPoolMgr수정 및 IngredientStation 기능 구현

--- Client/Header/CUtil.h
+++ Client/Header/CUtil.h
@@ -6,23 +6,23 @@ class CUtil
 {
 public:
     template<typename T>
-    static T Make_Random(T _rangeStart, T _rangeEnd) {
-        static random_device rd;
-        static mt19937 gen(rd());
-
-        if (is_integral<T>::value) {
-            uniform_int_distribution<T> dist(_rangeStart, _rangeEnd);
-            return dist(gen);
-        }
-        else if (is_floating_point<T>::value) {
-            uniform_real_distribution<T> dist(_rangeStart, _rangeEnd);
-            return dist(gen);
-        }
-        else {
-            static_assert(is_arithmetic<T>::value, "Make_Random only supports arithmetic types");
-            return T();
-        }
-    }
+    //static T Make_Random(T _rangeStart, T _rangeEnd) {
+    //    static random_device rd;
+    //    static mt19937 gen(rd());
+    //
+    //    if (is_integral<T>::value) {
+    //        uniform_int_distribution<T> dist(_rangeStart, _rangeEnd);
+    //        return dist(gen);
+    //    }
+    //    else if (is_floating_point<T>::value) {
+    //        uniform_real_distribution<T> dist(_rangeStart, _rangeEnd);
+    //        return dist(gen);
+    //    }
+    //    else {
+    //        static_assert(is_arithmetic<T>::value, "Make_Random only supports arithmetic types");
+    //        return T();
+    //    }
+    //}
 
     static wstring StringToWString(const std::string& str)
     {
