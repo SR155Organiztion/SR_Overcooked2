edaca4e61ef3af286b2f4e34fe4e8e1c43f1daa5
JunHyeongAn <sure083080@gmail.com>
2025-07-12 19:03:54 +0900

    [Feat] : 이웃타일 찾기 적용 이웃 타일 찾기 적용

--- Client/Code/CSelectGameSystem.cpp
+++ Client/Code/CSelectGameSystem.cpp
@@ -46,6 +46,10 @@ HRESULT CSelectGameSystem::Parse_TileObjectData(CLayer* _pLayer, vector<S_TILE>*
 
             Parse_Position<CHexTile>(tile, &pGameObject);
 
+            pGameObject->Get_Component(ID_DYNAMIC, L"Com_Transform");
+
+            //pair<int, int> key = Calculate_AxialFromWorldPos()
+
             if (nullptr == pGameObject)
                 return E_FAIL;
             if (FAILED(_pLayer->Add_GameObject(szKey, pGameObject)))
@@ -54,6 +58,37 @@ HRESULT CSelectGameSystem::Parse_TileObjectData(CLayer* _pLayer, vector<S_TILE>*
     }
 
 	return S_OK;
+}
+
+pair<int, int> CSelectGameSystem::Calculate_AxialFromWorldPos(const _vec3& _vPos)
+{
+    float q = (2.f / 3.f * _vPos.x);
+    float r = (-1.f / 3.f * _vPos.x + sqrt(3.f) / 3.f * _vPos.z);
+
+    int iq = roundf(q);
+    int ir = roundf(r);
+
+    return { iq, ir };
+}
+
+void CSelectGameSystem::Find_NeighborTile()
+{
+    for (auto key : m_mapHexAxialTile) {
+        pair<int, int> center = key.first;
+
+        for (int i = 0; i < 6; ++i) {
+            int dq = HEX_DIRECTIONS[i][0];
+            int dr = HEX_DIRECTIONS[i][1];
+            pair<int, int> neighbor = { center.first + dq, center.second + dr };
+
+            auto it = m_mapHexAxialTile.find(neighbor);
+            if (it != m_mapHexAxialTile.end()) {
+                //it->second->Flip();
+                it = it;
+            }
+        }
+    }
+
 }
 
 template<typename T>
@@ -90,6 +125,28 @@ void CSelectGameSystem::Parse_Position(
                 COMPONENTID::ID_DYNAMIC, L"Com_Transform"
             )
             );
+
+    pTransform->Set_Pos(
+        _stTile.vPos.x
+        , _stTile.vPos.y
+        , _stTile.vPos.z
+    );
+}
+
+template<typename T>
+void CSelectGameSystem::Parse_HexTilePosition(S_TILE _stTile, CGameObject** _pGameObject)
+{
+    *_pGameObject = T::Create(m_pGraphicDev);
+    CTransform* pTransform =
+        dynamic_cast<CTransform*>(
+            (*_pGameObject)->Get_Component(
+                COMPONENTID::ID_DYNAMIC, L"Com_Transform"
+            )
+            );
+
+    _vec3 vHexPos;
+    pTransform->Get_Info(INFO_POS, &vHexPos);
+    Calculate_AxialFromWorldPos(vHexPos);
 
     pTransform->Set_Pos(
         _stTile.vPos.x
--- Client/Header/CSelectGameSystem.h
+++ Client/Header/CSelectGameSystem.h
@@ -13,12 +13,26 @@ private:
 	virtual ~CSelectGameSystem();
 
 private:
+	const int HEX_DIRECTIONS[6][2] = {
+		{+1,  0},
+		{+1, -1},
+		{ 0, -1},
+		{-1,  0},
+		{-1, +1},
+		{ 0, +1}
+	};
+
 	LPDIRECT3DDEVICE9 m_pGraphicDev = nullptr;
 	S_STAGE m_stCurrStageInfo;
 	const string m_szCurrStage = "SelectMap";
 
+	unordered_map<pair<int, int>, CHexTile*> m_mapHexAxialTile;
+
 private:
 	HRESULT Parse_TileObjectData(CLayer* _pLayer, vector<S_TILE>* _pVecTile);
+	pair<int, int> Calculate_AxialFromWorldPos(const _vec3& _vPos);
+	void Find_NeighborTile();
+
 
 public:
 	HRESULT Ready_CSelectGameSystem(string _szCurrStage, LPDIRECT3DDEVICE9 _pGraphicDev, CScene* _pScene);
@@ -30,5 +44,8 @@ private:
 
 	template<typename T>
 	void		Parse_Position(S_TILE _stTile, CGameObject** _pGameObject);
+	
+	template<typename T>
+	void		Parse_HexTilePosition(S_TILE _stTile, CGameObject** _pGameObject);
 };
 
