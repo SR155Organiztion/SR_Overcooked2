06c374cbfc7dcb2e88d37c8084f0c5431f01a605
JunHyeongAn <sure083080@gmail.com>
2025-07-16 02:50:59 +0900

    [Feat] :주변 타일찾기 진행중

--- Client/Code/CSelectGameSystem.cpp
+++ Client/Code/CSelectGameSystem.cpp
@@ -1,7 +1,6 @@
 #include "pch.h"
 #include "CSelectGameSystem.h"
 #include "CTransform.h"
-#include "CHexTile.h"
 
 IMPLEMENT_SINGLETON(CSelectGameSystem)
 
@@ -50,6 +49,21 @@ HRESULT CSelectGameSystem::Parse_TileObjectData(CLayer* _pLayer, vector<S_TILE>*
 
             Parse_Position<CHexTile>(tile, &pGameObject);
 
+            // 타일 맵에 넣을 키값 생성
+            _tchar szTileKey[128] = L"";
+
+            swprintf_s(szTileKey, L"%.2f,%.2f,%.2f"
+                , tile.vPos.x, tile.vPos.y, tile.vPos.z);
+
+            size_t tileLen = wcslen(szTileKey) + 1;
+            _tchar* tileKey = new _tchar[tileLen];
+            wcscpy_s(tileKey, tileLen, szTileKey);
+
+            m_hexTileMap.insert({ 
+                tileKey
+                , dynamic_cast<CHexTile*>(pGameObject)
+            });
+
             if (nullptr == pGameObject)
                 return E_FAIL;
             if (FAILED(_pLayer->Add_GameObject(pKey, pGameObject)))
@@ -57,7 +71,45 @@ HRESULT CSelectGameSystem::Parse_TileObjectData(CLayer* _pLayer, vector<S_TILE>*
         }
     }
 
+    _vec3 vTest = { 1.5, -0.25, 0.f };
+
+    Find_Neighbor(&vTest);
+    Flip_Tile();
+
 	return S_OK;
+}
+
+void CSelectGameSystem::Find_Neighbor(_vec3* _vCenterPos)
+{
+    for (int i = 0; i < RANGE_END; i++) {
+        for (int j = 0; j < DIR_END; j++) {
+            _tchar szTileKey[128] = L"";
+
+            swprintf_s(szTileKey, L"%.2f,%.2f,%.2f"
+                , _vCenterPos->x + m_dirVec[j].x * (m_fRadius * (i + 1))
+                , _vCenterPos->y + m_dirVec[j].y * (m_fRadius * (i + 1))
+                , _vCenterPos->z + m_dirVec[j].z * (m_fRadius * (i + 1)));
+
+            auto iter = m_hexTileMap.find(szTileKey);
+
+            if (iter == m_hexTileMap.end()) {
+               MSG_BOX("not find");
+            }
+            else {
+                m_willFlipHexVec[i].push_back((*iter).second);
+            }
+        }
+    }
+}
+
+void CSelectGameSystem::Flip_Tile()
+{
+    for (int i = 0; i < RANGE_END; i++) {
+        for (int j = 0; j < m_willFlipHexVec[i].size(); j++) {
+            m_willFlipHexVec[i][j]->Flip();
+        }
+    }
+
 }
 
 template<typename T>
--- Client/Header/CSelectGameSystem.h
+++ Client/Header/CSelectGameSystem.h
@@ -3,10 +3,29 @@
 #include "CLayer.h"
 #include "CMapTool.h"
 #include "CScene.h"
+#include "CHexTile.h"
 
 class CSelectGameSystem
 {
 	DECLARE_SINGLETON(CSelectGameSystem)
+
+private:
+	enum HEX_DIRECTION {
+		UP
+		, RIGHT_UP
+		, RIGHT_DOWN
+		, DOWN
+		, LEFT_DOWN
+		, LEFT_UP
+		, DIR_END
+	};
+
+	enum WILL_FLIPED_TILE_RANGE {
+		FIRST_RANGE
+		, SECOND_RANGE
+		, THIRD_RANGE
+		, RANGE_END
+	};
 
 private:
 	explicit CSelectGameSystem();
@@ -16,9 +35,25 @@ private:
 	LPDIRECT3DDEVICE9 m_pGraphicDev = nullptr;
 	S_STAGE m_stCurrStageInfo;
 	const string m_szCurrStage = "SelectMap";
+	unordered_map<const _tchar*, CHexTile*> m_hexTileMap;
+
+	_vec3 m_dirVec[DIR_END] = {
+		{1, -1, 0},
+		{1, 0, -1},
+		{0, 1, -1},
+		{-1, 1, 0},
+		{-1, 0, 1},
+		{0, -1, 1}
+	};
+
+	vector<CHexTile*> m_willFlipHexVec[RANGE_END];
+
+	const _float m_fRadius = 1.f;
 
 private:
 	HRESULT Parse_TileObjectData(CLayer* _pLayer, vector<S_TILE>* _pVecTile);
+	void	Find_Neighbor(_vec3* _vCenterPos);
+	void	Flip_Tile();
 
 public:
 	HRESULT Ready_CSelectGameSystem(string _szCurrStage, LPDIRECT3DDEVICE9 _pGraphicDev, CScene* _pScene);
