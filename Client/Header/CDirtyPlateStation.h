936e53ffb7b24ce5d34929f7a0b0afddec219151
yeji-gwon <ehclemf0526@gmail.com>
2025-07-16 21:22:44 +0900

    [Feat] : 서빙 후 돌아오는 접시 쌓기
    - 서빙 이후에 돌아오는 접시를 쌓게 구현
    - 제일 먼저 들어온 접시는 위에, 마지막에 들어온 접시는 아래에 쌓임
    - 쌓인 접시에 재료를 놓으면 맨 위에 접시에 올라감

--- Client/Header/CDirtyPlateStation.h
+++ Client/Header/CDirtyPlateStation.h
@@ -33,11 +33,14 @@ public:
 	INTERACTTYPE	Get_InteractType() const override { return CInteract::STATION; }
 
 	// IPlace을(를) 통해 상속됨
-	_bool			Get_CanPlace(CGameObject* pItem) override;
+	_bool			Set_Place(CGameObject* pItem, CGameObject* pPlace) override { return false;  }
+	_bool			Get_CanPlace(CGameObject* pItem) override { return false; }
+	CGameObject*	Get_PlacedItem() override;
 
 private:
 	HRESULT			Add_Component();
 	void			Return_Plate(const _float& fTimeDelta);
+	void			Update_PlatePosition();
 
 private:
 	Engine::CCubeTex* m_pBufferCom;
@@ -49,11 +52,13 @@ private:
 	const _float	m_fInterval = 3.f;
 	_float			m_fTime = 0.f;
 
+	vector<CGameObject*>	m_vecItem;
+
 public:
 	static CDirtyPlateStation* Create(LPDIRECT3DDEVICE9 pGraphicDev);
 	// create후 어떤상자인지 세팅
 	void	Set_TypePlateStation(const _tchar* create_name);
 
 private:
-	virtual		void			Free(); 
+	virtual		void			Free();  
 };
