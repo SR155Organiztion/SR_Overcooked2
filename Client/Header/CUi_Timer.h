905d7be090f35db63180e4a7a105296eae283c19
beobi389 <ing5906@naver.com>
2025-07-22 17:11:57 +0900

    [Fix] 타이머 시간 오류 수정
    타이머 시간 오류 수정

--- Client/Header/CUi_Timer.h
+++ Client/Header/CUi_Timer.h
@@ -10,7 +10,8 @@ private:
 	LPD3DXFONT m_pFont;
 	LPD3DXSPRITE m_pSprite;
 
-	float m_fAccTime = 0.f;
+	_float m_dwTime = 0.f;
+
 
 public:
 	CUi_Timer();
@@ -25,7 +26,7 @@ public:
 	void Render_GameObject();
 	HRESULT Add_Component();
 	void Set_Timer(DWORD _dwLimitTime);
-	const DWORD Get_Timer() { return m_tData.m_dwTime; }
+	const _float Get_Timer() { return m_dwTime; }
 
 protected:
 	void Free();
