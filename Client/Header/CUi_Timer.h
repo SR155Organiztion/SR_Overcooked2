241003ad726b0ad47f94c7876755e1ede5af1024
beobi389 <ing5906@naver.com>
2025-07-22 14:39:01 +0900

    [Feat] 타이머 get 함수 추가
    타이머 get 함수 추가

--- Client/Header/CUi_Timer.h
+++ Client/Header/CUi_Timer.h
@@ -10,6 +10,8 @@ private:
 	LPD3DXFONT m_pFont;
 	LPD3DXSPRITE m_pSprite;
 
+	float remaining;
+
 public:
 	CUi_Timer();
 	CUi_Timer(LPDIRECT3DDEVICE9 pGraphicDev);
@@ -23,6 +25,7 @@ public:
 	void Render_GameObject();
 	HRESULT Add_Component();
 	void Set_Timer(DWORD _dwLimitTime);
+	float Get_Timer() { return remaining; }
 
 protected:
 	void Free();
