2de6ef3a8038150f29ab286f315bc5f5e5deae76
JunHyeongAn <sure083080@gmail.com>
2025-07-24 14:30:52 +0900

    [Fix] : 스테이지 시작시 종소리 제거

--- Client/Header/CInGameSystem.h
+++ Client/Header/CInGameSystem.h
@@ -46,7 +46,7 @@ private:
 	S_STAGE m_stCurrStageInfo;
 
 	_float m_fOrderTImeInterval = 8.f;
-	_float m_fOrderTimeElapsed = m_fOrderTImeInterval;
+	_float m_fOrderTimeElapsed = m_fOrderTImeInterval - 1.f;
 
 public:
 	HRESULT Ready_CInGameSystem(string _szCurrStage, LPDIRECT3DDEVICE9 _pGraphicDev, CScene* _pScene);
